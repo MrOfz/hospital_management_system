b035a4381120a3291971f96d63b1443c653bbb8f
zy <zyjy_ing@163.com>
2023-05-28 13:06:12 +0800

    hot-fix commit1

--- src/test/java/com/zy/test/Test1.java
+++ src/test/java/com/zy/test/Test1.java
@@ -16,7 +16,7 @@ public class Test1 {
 //        String s2 = "nowcoder";
 //        System.out.println(s1 == s2);
 //        System.out.println(s1.equals(s2));
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList();
         for (int i = 1; i < 100; i++) {
             list.add(i);
         }
@@ -24,5 +24,6 @@ public class Test1 {
         int pageSize = 10;
         System.out.println(list.subList((page - 1) * pageSize, page * pageSize));
         System.out.println("test222");
+        System.out.println("testOfHotFix");
     }
 }
