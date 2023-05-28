22d5d2074ff6cf7b1034707cfd4802f84c46f3ff
zy <zyjy_ing@163.com>
2023-05-28 14:19:06 +0800

    push commit

--- src/test/java/com/zy/test/Test1.java
+++ src/test/java/com/zy/test/Test1.java
@@ -27,5 +27,6 @@ public class Test1 {
         System.out.println("testOfHotFix");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
